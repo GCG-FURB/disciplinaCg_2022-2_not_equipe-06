10953223b1396a939884270c1243b40db40d6f99
Helio <helio.p2000@hotmail.com>
2022-11-25 23:03:53 -0300

    Light object

---
+++ unidade_4/CG_N4/Light.cs
@@ -0,0 +1,68 @@
+using OpenTK.Graphics.OpenGL;
+using CG_Biblioteca;
+
+namespace gcgcg
+{
+    internal class Light : ObjetoGeometria
+    {
+        private bool exibeVetorNormal = false;
+        public Light(char rotulo, Objeto paiRef) : base(rotulo, paiRef) { }
+
+        protected override void DesenharObjeto()
+        {
+            GL.Color3(OpenTK.Color.White);
+            GL.Begin(PrimitiveType.Quads);
+
+            // Face da frente
+            GL.Normal3(0, 0, 1);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            // Face do fundo
+            GL.Normal3(0, 0, -1);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+            // Face de cima
+            GL.Normal3(0, 1, 0);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            // Face de baixo
+            GL.Normal3(0, -1, 0);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+            // Face da direita
+            GL.Normal3(1, 0, 0);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(1.0f, 1.0f, 1.0f);
+            // Face da esquerda
+            GL.Normal3(-1, 0, 0);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, -1.0f);
+
+            GL.End();
+        }
+
+        public override string ToString()
+        {
+            string retorno;
+            retorno = "__ Objeto Cubo: " + base.rotulo + "\n";
+            for (var i = 0; i < pontosLista.Count; i++)
+            {
+                retorno += "P" + i + "[" + pontosLista[i].X + "," + pontosLista[i].Y + "," + pontosLista[i].Z + "," + pontosLista[i].W + "]" + "\n";
+            }
+            return (retorno);
+        }
+
+    }
+}
